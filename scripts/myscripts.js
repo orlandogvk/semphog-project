65ed83a715be3befd66ef896c3bbe912b2fe3ada
Orlando Rodriguez <orlandog79@gmail.com>
2020-02-09 17:02:23 -0500

    Pagina de inicio QuienesSomos.html del proyecto Semphog en Materialize

---
+++ scripts/myscripts.js
@@ -0,0 +1,12 @@
+ /*Para poder activar el menu desplegable responsive JQuery*/
+ $(document).ready(function () {
+    $(".button-collapse").sideNav();
+})
+/*Para poder correr el slider JQuery*/
+$(document).ready(function () {
+    $('.slider').slider({indicators:false,
+    height:600});
+});
+
+
+
